cb5ba92b259965e07c8e48388110cda714fd00d7
jhersbo <jhersbo@gmail.com>
2021-11-29 20:54:59 -0600

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+let http = require('http')
+let server = http.createServer(function (req, res) {
+    res.writeHeader(200)
+    res.write('<h1 style="color: blue">Hello FSD!</h1>')
+    res.end()
+    
+})
+
+server.listen(3000, () => {
+    console.log('Yucky')
+})
